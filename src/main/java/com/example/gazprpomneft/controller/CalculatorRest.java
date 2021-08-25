c099e6711e48df16f6110a696e6957605f3bcdc6
Sergei Volkov <volkovself@gmail.com>
2021-08-25 21:47:34 +0300

    Add Swagger, fix REST responce like dneonlene.com

--- src/main/java/com/example/gazprpomneft/controller/CalculatorRest.java
+++ src/main/java/com/example/gazprpomneft/controller/CalculatorRest.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
 import java.util.Map;
 
 @RestController
@@ -17,23 +18,27 @@ public class CalculatorRest {
     public final CalculatorClient client;
 
     @PostMapping(value = "/add")
-    public int add(@RequestBody Map<String, Integer> data) {
-        return client.calcAdd(data.get("intA"), data.get("intB")).getAddResult();
+    public Map<String, Integer> add(@RequestBody Map<String, Integer> data) {
+        return Collections.singletonMap("AddResult",
+                client.calcAdd(data.get("intA"), data.get("intB")).getAddResult());
     }
 
     @PostMapping(value = "/divide")
-    public int divide(@RequestBody Map<String, Integer> data) {
-        return client.calcDivide(data.get("intA"), data.get("intB")).getDivideResult();
+    public Map<String, Integer> divide(@RequestBody Map<String, Integer> data) {
+        return Collections.singletonMap("DivideResult",
+                client.calcDivide(data.get("intA"), data.get("intB")).getDivideResult());
     }
 
     @PostMapping(value = "/multiply")
-    public int multiply(@RequestBody Map<String, Integer> data) {
-        return client.calcMultiply(data.get("intA"), data.get("intB")).getMultiplyResult();
+    public Map<String, Integer> multiply(@RequestBody Map<String, Integer> data) {
+        return Collections.singletonMap("Multiply",
+                client.calcMultiply(data.get("intA"), data.get("intB")).getMultiplyResult());
     }
 
     @PostMapping(value = "/subtract")
-    public int subtract(@RequestBody Map<String, Integer> data) {
-        return client.calcSubtract(data.get("intA"), data.get("intB")).getSubtractResult();
+    public Map<String, Integer> subtract(@RequestBody Map<String, Integer> data) {
+        return Collections.singletonMap("SubtractResult",
+                client.calcSubtract(data.get("intA"), data.get("intB")).getSubtractResult());
     }
 
 }
